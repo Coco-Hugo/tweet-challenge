07dab327e46a64350f2cd41a77a7e2ebd3280f46
Kyle Kim <kyle.kim@nativept.kr>
2024-11-09 04:07:42 +0900

    refactored

--- tweets/urls.py
+++ tweets/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/v1/tweets/', views.show_all_tweets),
-    path('api/v1/users/<int:user_id>/tweets/', views.show_all_user_tweets),
+    path('api/v1/tweets/', views.TweetsView.as_view()),
+    path('api/v1/users/<int:user_id>/tweets/', views.UserTweetsView.as_view()),
 ]
--- tweets/views.py
+++ tweets/views.py
@@ -2,22 +2,40 @@ from users.models import User
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.exceptions import NotFound
+from rest_framework.views import APIView
 from .models import Tweet
 from .serializers import TweetSerializer
 
-@api_view(["GET"])
-def show_all_tweets(request):
-    tweets = Tweet.objects.all()
-    serializer = TweetSerializer(tweets, many=True)
-    return Response(serializer.data)
+class TweetsView(APIView):
+    def get(self, request):
+        tweets = Tweet.objects.all()
+        serializer = TweetSerializer(tweets, many=True)
+        return Response(serializer.data) 
 
-@api_view(["GET"])
-def show_all_user_tweets(request, user_id):
-    try:
-        user = User.objects.get(pk=user_id)
-    except User.DoesNotExist:
-        raise NotFound(detail="User not found")
+class UserTweetsView(APIView):
+    def get(self, request, user_id):
+        try:
+            user = User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            raise NotFound(detail="User not found")
 
-    user_tweets = user.tweets.all()  # Access user's tweets using related_name
-    serializer = TweetSerializer(user_tweets, many=True)
-    return Response(serializer.data)
+        user_tweets = user.tweets.all()  # Access user's tweets using related_name
+        serializer = TweetSerializer(user_tweets, many=True)
+        return Response(serializer.data)  
+
+# @api_view(["GET"])
+# def show_all_tweets(request):
+#     tweets = Tweet.objects.all()
+#     serializer = TweetSerializer(tweets, many=True)
+#     return Response(serializer.data)
+
+# @api_view(["GET"])
+# def show_all_user_tweets(request, user_id):
+#     try:
+#         user = User.objects.get(pk=user_id)
+#     except User.DoesNotExist:
+#         raise NotFound(detail="User not found")
+
+#     user_tweets = user.tweets.all()  # Access user's tweets using related_name
+#     serializer = TweetSerializer(user_tweets, many=True)
+#     return Response(serializer.data)
