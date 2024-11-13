073b8697a0236576089095b50c4b3278832961aa
Kyle Kim <kyle.kim@nativept.kr>
2024-11-14 05:54:06 +0900

    user settings

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('api/v1/users/', views.UsersView.as_view()),
     path('api/v1/users/<int:pk>/', views.UserDetailsView.as_view()),
+    path('api/v1/users/password', views.EditPassword.as_view()),
+    path('api/v1/users/login', views.LogIn.as_view()),
+    path('api/v1/users/logout', views.LogOut.as_view()),
 ]
--- users/views.py
+++ users/views.py
@@ -1,12 +1,16 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.exceptions import NotFound
+from rest_framework.exceptions import NotFound, ParseError
 from rest_framework.status import HTTP_204_NO_CONTENT
+from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+
 from .models import User
 from .serializers import UserSerializer
 
 from django.db import transaction
+from django.contrib.auth import authenticate, login, logout
 
 
 class UsersView(APIView):
@@ -16,14 +20,17 @@ class UsersView(APIView):
         return Response(serializer.data) 
     
     def post(self, request):
-        if request.user.is_authenticated:
-            serializer = UserSerializer(data=request.data)
-            if serializer.is_valid():
-                with transaction.atomic():
-                    user = serializer.save()
-                    return Response(UserSerializer(user).data)
-            else:
-                return Response(serializer.errors)
+        password = request.data.get("password")
+        if not password:
+            raise ParseError
+        serializer = UserSerializer(data=request.data, partial=True)
+        if serializer.is_valid():
+            new_user = serializer.save()
+            new_user.set_password(password)# Never do this: new_user.password = password (unhashed)
+            serializer = UserSerializer(new_user)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
         
 
 class UserDetailsView(APIView):
@@ -53,3 +60,40 @@ class UserDetailsView(APIView):
             raise NotAuthenticated
         user.delete()
         return Response(status=HTTP_204_NO_CONTENT)
+
+
+class EditPassword(APIView):
+    permission_classes = [IsAuthenticated]
+    def post(self, request):
+        user = request.user
+        old_pw = request.data.get("old_password")
+        new_pw = request.data.get("new_password")
+        if not old_pw or not new_pw:
+            raise ParseError
+        if user.check_password(old_pw):
+            user.set_password(new_pw)
+            user.save()
+            return Response(status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+            
+
+class LogIn(APIView):
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        if not username or not password:
+            raise ParseError
+        user = authenticate(request, username=username, password=password)
+        if user:
+            login(request, user) # Create session and send cookies to user
+            return Response({"ok": "Welcome!"})
+        else:
+            return Response({"error": "Wrong Password"})
+        
+        
+class LogOut(APIView):
+    permission_classes = [IsAuthenticated]
+    def post(self, request):
+        logout(request)
+        return Response({"ok":"Goodbye"})
